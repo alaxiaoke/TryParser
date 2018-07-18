98b9e8b72fea7bf485f61ef70a513ba5ddaf13a5
alaxiaoke <2356120327@qq.com>
2018-07-18 20:42:45 +0800

    第一次提交的tryparser

---
+++ src/main/java/zou_z_l/weighted_doxy/TryParser2.java
@@ -0,0 +1,26 @@
+package zou_z_l.weighted_doxy;
+import java.io.*;   
+import java.util.*;   
+import org.dom4j.*;   
+import org.dom4j.io.*;   
+
+
+public class TryParser2 {
+	  public static void main(String arge[]) {   
+		   long lasting = System.currentTimeMillis();   
+		   try {   
+		     File f = new File("data_10k.xml");   
+		     SAXReader reader = new SAXReader();   
+		     Document doc = reader.read(f);   
+		     Element root = doc.getRootElement();   
+		     Element foo;   
+		    for (Iterator i = root.elementIterator("VALUE"); i.hasNext();) {   
+		      foo = (Element) i.next();   
+		      System.out.print("车牌号码:" + foo.elementText("NO"));   
+		      System.out.println("车主地址:" + foo.elementText("ADDR"));   
+		     }   
+		    } catch (Exception e) {   
+		     e.printStackTrace();   
+		    }   
+		 }   
+}
